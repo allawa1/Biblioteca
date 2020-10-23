f602a61ac01289f351a4292745b3b8c9ab69056e
allawa1 <alexiaallaway@gmail.com>
2020-10-22 20:14:22 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use("/css", express.static(__dirname + "/css"))
+app.use("/js", express.static(__dirname + "/js"))
+app.use("/", express.static(__dirname + "/public"))
+
+app.get('/heartbeat',(req,res)=>{
+    res.json({
+        is: "working"
+    })
+})
+
+app.listen(3001, ()=> {
+    console.log('The server is running at Port 3001')
+})
